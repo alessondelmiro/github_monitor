964efeb5367b68c25856349d23e9a30709567a56
alesson_delmiro <alesson.delmiro@bemind.com.br>
2020-05-17 14:56:39 -0300

    Adding logou button + some fixes

--- frontend/js/app/components/Common/Header.js
+++ frontend/js/app/components/Common/Header.js
@@ -2,20 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const logout = () => {
+    localStorage.clear();
+    window.location.replace('/login');
+  };
+
   return (
-    <nav className="navbar navbar-dark bg-dark d-flex justify-content-start">
+    <nav className="navbar navbar-dark bg-dark d-flex justify-content-between">
       <Link to="/">
         <div className="navbar-brand">GitHub Monitor</div>
       </Link>
-      <div>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link to="/">
-              <div className="nav-link">Home</div>
-            </Link>
-          </li>
-        </ul>
-      </div>
+      <button className="btn btn-sm btn-secondary" type="button" onClick={logout}>
+        Logout
+      </button>
     </nav>
   );
 };
--- frontend/js/app/components/Repository/CommitList.js
+++ frontend/js/app/components/Repository/CommitList.js
@@ -6,42 +6,44 @@ const CommitList = ({ detail, commits }) => {
   return (
     <div className="list-container">
       {commits && commits.length > 0 ? (
-        <table className="table table-fix">
-          <thead className="thead-light">
-            <th className="author-col" scope="col">
-              Author
-            </th>
-            <th className="commit-col" scope="col">
-              Commit
-            </th>
-            <th scope="col">Message</th>
-            {detail ? null : (
-              <th className="repo-col" scope="col">
-                Repository
+        <div className="table-responsive">
+          <table className="table table-fix">
+            <thead className="thead-light">
+              <th className="author-col" scope="col">
+                Author
               </th>
-            )}
-            <th className="date-col" scope="col">
-              Date
-            </th>
-          </thead>
-          <tbody>
-            {commits.map((commit) => (
-              <tr key={commit.id}>
-                <th className="author-row" scope="row">
-                  {commit.author}
+              <th className="commit-col" scope="col">
+                Commit
+              </th>
+              <th scope="col">Message</th>
+              {detail ? null : (
+                <th className="repo-col" scope="col">
+                  Repository
                 </th>
-                <td>{commit.commit}</td>
-                <td className="message-col">{commit.message}</td>
-                {detail ? null : (
-                  <Link to={`/repository/${commit.repository.id}`}>
-                    <td>{commit.repository.name}</td>
-                  </Link>
-                )}
-                <td>{commit.date}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+              )}
+              <th className="date-col" scope="col">
+                Date
+              </th>
+            </thead>
+            <tbody>
+              {commits.map((commit) => (
+                <tr key={commit.id}>
+                  <th className="author-row" scope="row">
+                    {commit.author}
+                  </th>
+                  <td>{commit.commit}</td>
+                  <td className="message-col">{commit.message}</td>
+                  {detail ? null : (
+                    <Link to={`/repository/${commit.repository.id}`}>
+                      <td>{commit.repository.name}</td>
+                    </Link>
+                  )}
+                  <td>{commit.date}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       ) : (
         <div className="d-flex flex-column align-items-center">
           <h4 className="title">Looks like you have not committed for the last 30 days</h4>
